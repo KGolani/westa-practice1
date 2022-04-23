7f074beb0c076cee6957327fba22e7bc304a8e28
전예찬 맥북에어 <yechan@jeon-yechan-ui-MacBookAir.local>
2022-04-23 17:44:02 +0900

    setting

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+SECRET_KEY = 'django-insecure-cuk_3u_*u!gj$f#q&29s=yd_1^_hp10bynwag%j+z=lb@zgrt$'
+
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'westatest2',
+        'USER': 'root',        #root
+        'PASSWORD': '0421',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
